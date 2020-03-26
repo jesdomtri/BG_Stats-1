edf01293363cbb774015717f93f4b81dea0a6259
jesdomtri <jesdomtri@gmail.com>
2020-03-26 13:14:15 +0100

    Test

--- app/src/main/java/com/example/bg_stats/DiceRollerActivity.java
+++ app/src/main/java/com/example/bg_stats/DiceRollerActivity.java
@@ -1,11 +1,17 @@
 package com.example.bg_stats;
 
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class DiceRollerActivity extends AppCompatActivity {
+
+    TextView diceCount;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -15,5 +21,23 @@ public class DiceRollerActivity extends AppCompatActivity {
 
         // Set view
         setContentView(R.layout.activity_dice);
+
+        RadioGroup rGroup = findViewById(R.id.radioGroup);
+        rGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton checkedRadioButton = group.findViewById(checkedId);
+            }
+        });
     }
+
+    public void onPlusNumberClick(View view) {
+        diceCount = findViewById(R.id.diceCount);
+        diceCount.setText(Integer.parseInt(diceCount.getText().toString()) + 1);
+    }
+
+    public void onLessNumberClick(View view) {
+        diceCount = findViewById(R.id.diceCount);
+        diceCount.setText(Integer.parseInt(diceCount.getText().toString()) - 1);
+    }
+
 }
