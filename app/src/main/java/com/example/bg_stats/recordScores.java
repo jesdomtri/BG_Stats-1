fa80a9f60651931c1a3cea2948bbe9b646375688
jesdomtri <jesdomtri@gmail.com>
2020-08-29 23:24:24 +0200

    Almost finished.

---
+++ app/src/main/java/com/example/bg_stats/recordScores.java
@@ -0,0 +1,14 @@
+package com.example.bg_stats;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class recordScores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_scores);
+    }
+}
